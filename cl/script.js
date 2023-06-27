e9bec1f65d2648557ce0fb72b90cb604a0fcf2ee
bjaashish <aashishb89@gmail.com>
2023-06-27 14:04:55 +0530

    server url added

--- cl/script.js
+++ cl/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-
+    // https://qbot-dodd.onrender.com/
     const response = await fetch('https://qbot-dodd.onrender.com/', {
         method: 'POST',
         headers: {
